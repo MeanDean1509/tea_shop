345fdc3fad788ded493207d1bcc840aee544e627
MeanDean1509 <castrovina555@gmail.com>
2025-11-11 01:07:12 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import {partnerLogos, productList, partnerLogoBasePath} from "./data.js";
+//  parnter logos
+
+
+$(function(){
+    const container = document.getElementById('partner-logo-list');
+    partnerLogos.forEach(logo => {
+        const img =  document.createElement('img');
+        img.src = partnerLogoBasePath + logo.fileName;
+        img.alt = logo.alt;
+        img.classList.add('logo-ticker-image');
+        container.appendChild(img);
+    });
+});
+
